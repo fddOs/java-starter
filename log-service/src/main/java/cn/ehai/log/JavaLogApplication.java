7af9e9d78d1cb29f0bdac27192f871e9bc664b10
27894 <1240262979@qq.com>
2018-12-20 16:11:51 +0800

    [fix]去掉mapperScan

--- log-service/src/main/java/cn/ehai/log/JavaLogApplication.java
+++ log-service/src/main/java/cn/ehai/log/JavaLogApplication.java
@@ -1,12 +1,10 @@
 package cn.ehai.log;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackageClasses = JavaLogApplication.class)
-@MapperScan(basePackages = "cn.ehai.log.dao")
 public class JavaLogApplication {
 
 }
--- spring-web/src/main/java/cn/ehai/web/config/EhiHttpServletResponseWrapper.java
+++ spring-web/src/main/java/cn/ehai/web/config/EhiHttpServletResponseWrapper.java
@@ -1,14 +1,10 @@
 package cn.ehai.web.config;
 
-
-
-
 import javax.servlet.ServletOutputStream;
 import javax.servlet.WriteListener;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.*;
-import org.apache.commons.io.output.TeeOutputStream;
 
 /**
  * @Description: TODO
@@ -32,48 +28,40 @@ public class EhiHttpServletResponseWrapper extends HttpServletResponseWrapper {
 
     @Override
     public ServletOutputStream getOutputStream()
-        throws IOException
-    {
+            throws IOException {
         return wrapperOutputStream;
     }
 
     @Override
     public void flushBuffer()
-        throws IOException
-    {
-        if (wrapperOutputStream != null)
-        {
+            throws IOException {
+        if (wrapperOutputStream != null) {
             wrapperOutputStream.flush();
         }
     }
 
     public byte[] getContent()
-        throws IOException
-    {
+            throws IOException {
         flushBuffer();
         return output.toByteArray();
     }
 
 
-    class WrapperOutputStream extends ServletOutputStream
-    {
+    class WrapperOutputStream extends ServletOutputStream {
         private ByteArrayOutputStream bos;
 
-        public WrapperOutputStream(ByteArrayOutputStream bos)
-        {
+        public WrapperOutputStream(ByteArrayOutputStream bos) {
             this.bos = bos;
         }
 
         @Override
         public void write(int b)
-            throws IOException
-        {
+                throws IOException {
             bos.write(b);
         }
 
         @Override
-        public boolean isReady()
-        {
+        public boolean isReady() {
 
             // TODO Auto-generated method stub
             return false;
@@ -81,8 +69,7 @@ public class EhiHttpServletResponseWrapper extends HttpServletResponseWrapper {
         }
 
         @Override
-        public void setWriteListener(WriteListener arg0)
-        {
+        public void setWriteListener(WriteListener arg0) {
 
             // TODO Auto-generated method stub
 
