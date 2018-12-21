c1a4ced6979ea1ba13ee4affcaa344d18ae4ea84
xianglong.chen✨windows <xianglong.chen@1hai.cn>
2018-12-21 16:32:08 +0800

    [功能]：子模块对自己范围内的 dao 进行扫描

--- log-service/src/main/java/cn/ehai/log/JavaLogApplication.java
+++ log-service/src/main/java/cn/ehai/log/JavaLogApplication.java
@@ -1,10 +1,12 @@
 package cn.ehai.log;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackageClasses = JavaLogApplication.class)
+@MapperScan(basePackages = "cn.ehai.log.dao")
 public class JavaLogApplication {
 
 }
