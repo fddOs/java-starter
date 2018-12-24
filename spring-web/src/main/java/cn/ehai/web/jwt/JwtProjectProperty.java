eba6a8e77f398f295c09c76cb5cc5d9196f90932
lixiao <xiao.li@1hai.cn>
2018-12-24 16:56:20 +0800

    [add]jwt添加业务排除验证的自有路径

---
+++ spring-web/src/main/java/cn/ehai/web/jwt/JwtProjectProperty.java
@@ -0,0 +1,51 @@
+package cn.ehai.web.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
+
+/**
+ * jwt配置
+ *
+ * @author lixiao
+ * @date 2018-12-24 16:33
+ */
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProjectProperty {
+
+    /**
+     * jwt拦截器不拦截的url 默认 druid、swagger、heartbeat
+     * @author lixiao
+     * @date 2018-12-24 16:37
+     */
+    @Value("interceptorUrl")
+    private String interceptorUrl;
+
+    private List<String> url = new ArrayList<>();
+
+    public String getInterceptorUrl() {
+        return interceptorUrl;
+    }
+
+    public void setInterceptorUrl(String interceptorUrl) {
+        this.interceptorUrl = interceptorUrl;
+        if(!StringUtils.isEmpty(interceptorUrl)){
+            String[] strings = interceptorUrl.split(",");
+            if(null!=strings&&strings.length>0){
+                for (String s:strings ) {
+                    url.add(s);
+                }
+            }
+        }
+    }
+
+    public List<String> getUrl() {
+        return url;
+    }
+
+    public void setUrl(List<String> url) {
+        this.url = url;
+    }
+}
