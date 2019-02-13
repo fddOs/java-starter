9c67a22fba324f994eea36626ffab104e292caf5
lixiao <xiao.li@1hai.cn>
2019-02-13 15:31:10 +0800

    fix(apollo):
    如果从apollo中返回null，返回默认值

--- common-core/src/main/java/cn/ehai/common/core/ApolloBaseConfig.java
+++ common-core/src/main/java/cn/ehai/common/core/ApolloBaseConfig.java
@@ -107,9 +107,11 @@ public class ApolloBaseConfig {
         if (StringUtils.isEmpty(key)) {
             throw new ServiceException(ResultCode.INTERNAL_SERVER_ERROR, "请求失败:获取配置key为空，请稍后重试");
         }
-        String str = null;
+        if(application==null){
+            return defaultValue;
+        }
+        String str = application.getProperty(key, defaultValue);
         try {
-            str = application.getProperty(key, defaultValue);
             str = AESUtils.aesDecryptString(str);
         } catch (Exception e) {
             str = defaultValue;
