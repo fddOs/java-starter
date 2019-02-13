c5e2afa018a2cbfb4a0b794747795072356cc209
lixiao <xiao.li@1hai.cn>
2019-02-13 14:53:07 +0800

    feat(加解密、签名):
    新增请求解密签名的过滤器、返回值签名加密的过滤器、优化result返回

--- spring-web/src/main/java/cn/ehai/web/config/EhiSignServletRequestWrapper.java
+++ spring-web/src/main/java/cn/ehai/web/config/EhiSignServletRequestWrapper.java
@@ -1,44 +1,132 @@
 package cn.ehai.web.config;
 
+import cn.ehai.common.core.ApolloBaseConfig;
 import cn.ehai.common.utils.EHIExceptionLogstashMarker;
 import cn.ehai.common.utils.EHIExceptionMsgWrapper;
-import cn.ehai.common.utils.LoggerUtils;
+import cn.ehai.common.core.ResultCode;
+import cn.ehai.common.core.ServiceException;
+import cn.ehai.common.utils.AESUtils;
+import cn.ehai.common.utils.SignUtils;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
+import cn.ehai.common.utils.LoggerUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.owasp.encoder.Encode;
 import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+
+import static cn.ehai.web.common.SignConfig.SIGN_HEADER;
+import static cn.ehai.web.config.VerificationReqFilter.REQ_DECODE;
+import static cn.ehai.web.config.VerificationReqFilter.REQ_SIGN;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 /**
- * TODO
- *
+ * 对请求参数进行解密
  * @author lixiao
- * @date 2018/12/17 17:00
+ * @date 2019-02-12 14:24
  */
 public class EhiSignServletRequestWrapper extends HttpServletRequestWrapper {
 
+    private byte[] requestBody;
 
-    private byte[] requestBody = null;
+    private Map<String, String[]> parameterMap;
+
+    private static final String CHARSE = "UTF-8";
+
 
     public EhiSignServletRequestWrapper(HttpServletRequest request) {
 
         super(request);
-        //缓存请求body
+        //解密url的参数
+        parameterMap = handlerQuestString(request.getQueryString());
+        //处理body的参数
+        String reqBody = handlerBodyParams(request);
         try {
-            requestBody = StreamUtils.copyToByteArray(request.getInputStream());
-        } catch (IOException e) {
-            LoggerUtils.error(getClass(), new EHIExceptionLogstashMarker(new EHIExceptionMsgWrapper
-                    (getClass().getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new
-                            Object[]{request}, ExceptionUtils.getStackTrace(e))));
+            requestBody =  reqBody.getBytes(CHARSE);
+        } catch (UnsupportedEncodingException e) {
+            LoggerUtils.error(getClass(), new EHIExceptionLogstashMarker(new EHIExceptionMsgWrapper(getClass()
+                .getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new Object[]{request},
+                ExceptionUtils.getStackTrace(e))));
+            throw new ServiceException(ResultCode.FAIL, e.getMessage());
         }
+
+        if (REQ_SIGN &&!signRequest(request, reqBody, getParams(parameterMap))) {
+            throw new ServiceException(ResultCode.UNAUTHORIZED, "签名错误");
+        }
+
+    }
+
+
+    /**
+     * 处理请求的body参数
+     * @param request
+     * @return java.lang.String
+     * @author lixiao
+     * @date 2019-02-12 14:44
+     */
+    private String handlerBodyParams(HttpServletRequest request){
+        String reqBody=null;
+        //处理除get请求外的body里面的参数
+        if(!GET.name().equals(request.getMethod())){
+            //缓存请求body
+            try {
+                reqBody = aesDecrypt(StreamUtils.copyToString(request.getInputStream(),
+                    Charset.forName(CHARSE)));
+            } catch (Exception e) {
+                LoggerUtils.error(getClass(), new EHIExceptionLogstashMarker(new EHIExceptionMsgWrapper(getClass()
+                    .getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new Object[]{request},
+                    ExceptionUtils.getStackTrace(e))));
+                throw new ServiceException(ResultCode.UNAUTHORIZED, "body参数解密错误");
+            }
+        }
+        if (reqBody == null) {
+            reqBody="";
+        }
+        return reqBody;
+    }
+
+    private Map<String, String> getParams(Map<String, String[]> parameter) {
+        Map<String, String> parameterMap = new HashMap<>();
+        Iterator<Map.Entry<String, String[]>> iterator = parameter.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String[]> entry = iterator.next();
+            parameterMap.put(entry.getKey(), entry.getValue()[0]);
+        }
+        return parameterMap;
+    }
+
+
+    private boolean signRequest(HttpServletRequest request, String requestBody, Map<String, String> query) {
+
+        if (StringUtils.isEmpty(requestBody) && query.isEmpty()) {
+            return true;
+        }
+        String sign = SignUtils.sign(query, requestBody);
+        String signHeader = request.getHeader(SIGN_HEADER);
+        if (StringUtils.isEmpty(signHeader) || StringUtils
+                .isEmpty(sign)) {
+            return false;
+        }
+
+        return signHeader.equalsIgnoreCase(sign);
     }
 
     /**
@@ -58,22 +146,35 @@ public class EhiSignServletRequestWrapper extends HttpServletRequestWrapper {
 
             @Override
             public boolean isFinished() {
-                // TODO Auto-generated method stub
                 return false;
             }
 
             @Override
             public boolean isReady() {
-                // TODO Auto-generated method stub
                 return false;
             }
 
             @Override
             public void setReadListener(ReadListener listener) {
-                // TODO Auto-generated method stub
 
             }
         };
+    }
+
+    /**
+     * AES解密
+     *
+     * @param string
+     * @return java.lang.String
+     * @author lixiao
+     * @date 2018/12/15 15:49
+     */
+    private String aesDecrypt(String string) {
+        try {
+            return AESUtils.aesDecryptString(string);
+        } catch (Exception e) {
+            throw new ServiceException(ResultCode.UNAUTHORIZED, "参数解密错误");
+        }
     }
 
     /**
@@ -84,24 +185,94 @@ public class EhiSignServletRequestWrapper extends HttpServletRequestWrapper {
         return new BufferedReader(new InputStreamReader(getInputStream()));
     }
 
-    @Override
-    public String[] getParameterValues(String name) {
 
-        String[] values = super.getParameterValues(name);
-        if (values != null) {
-            int length = values.length;
-            String[] escapseValues = new String[length];
-            for (int i = 0; i < length; i++) {
-                escapseValues[i] = Encode.forHtmlContent(values[i]);
-            }
-            return escapseValues;
+    /**
+     * 解密url后面的参数
+     * @param questSting
+     * @return java.util.Map<java.lang.String,java.lang.String[]>
+     * @author lixiao
+     * @date 2019-02-12 15:50
+     */
+    private Map<String, String[]> handlerQuestString(String questSting) {
+        Map<String, String[]> paramsMap = new HashMap();
+        if (StringUtils.isEmpty(questSting)) {
+            return paramsMap;
+        }
+        //是否解密
+
+        try {
+            //对请求url的参数进行url解码
+            questSting = URLDecoder.decode(questSting, CHARSE);
+        } catch (UnsupportedEncodingException e) {
+            throw new ServiceException(ResultCode.FAIL, "URLDecoder解码失败");
         }
 
-        return super.getParameterValues(name);
+        try {
+            //对请求url的参数进行解密
+            String params ;
+            if(REQ_DECODE){
+                params = aesDecrypt(questSting);
+            }else{
+                params = questSting;
+            }
+            if (!StringUtils.isEmpty(params)) {
+                String[] paramList = params.split("&");
+                for (String param : paramList) {
+                    String[] string = param.split("=");
+                    if (string.length == 2 && !StringUtils.isEmpty(string[1])) {
+                        paramsMap.put(string[0], new String[]{string[1]});
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LoggerUtils.error(getClass(), new EHIExceptionLogstashMarker(new EHIExceptionMsgWrapper(getClass()
+                    .getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new Object[]{questSting},
+                    ExceptionUtils.getStackTrace(e))));
+            throw new ServiceException(ResultCode.FAIL, "Aes解密失败");
+        }
+
+        return paramsMap;
     }
 
+    // 重写几个HttpServletRequestWrapper中的方法
+
+    /**
+     * 获取所有参数名
+     *
+     * @return 返回所有参数名
+     */
     @Override
-    public String getHeader(String name) {
-        return super.getHeader(name);
+    public Enumeration<String> getParameterNames() {
+        Vector<String> vector = new Vector<String>(parameterMap.keySet());
+        return vector.elements();
+    }
+
+    /**
+     * 获取指定参数名的值，如果有重复的参数名，则返回第一个的值 接收一般变量 ，如text类型
+     *
+     * @param name 指定参数名
+     * @return 指定参数名的值
+     */
+    @Override
+    public String getParameter(String name) {
+        String[] results = parameterMap.get(name);
+        if (results == null || results.length <= 0) {
+            return "";
+        } else {
+            return results[0];
+        }
+    }
+
+    /**
+     * 获取指定参数名的所有值的数组  如果参数重名后面的参数会覆盖前面的参数
+     */
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] results = parameterMap.get(name);
+        if (results == null || results.length <= 0) {
+            return new String[]{};
+        } else {
+            return results;
+        }
     }
 }
--- spring-web/src/main/java/cn/ehai/web/config/VerificationResFilter.java
+++ spring-web/src/main/java/cn/ehai/web/config/VerificationResFilter.java
@@ -1,18 +1,17 @@
 package cn.ehai.web.config;
 
-import cn.ehai.common.utils.EHIExceptionLogstashMarker;
-import cn.ehai.common.utils.EHIExceptionMsgWrapper;
 import cn.ehai.common.core.Result;
 import cn.ehai.common.core.ResultCode;
 import cn.ehai.common.core.ResultGenerator;
 import cn.ehai.common.core.ServiceException;
 import cn.ehai.common.utils.AESUtils;
+import cn.ehai.common.utils.EHIExceptionLogstashMarker;
+import cn.ehai.common.utils.EHIExceptionMsgWrapper;
 import cn.ehai.common.utils.IOUtils;
 import cn.ehai.common.utils.LoggerUtils;
 import cn.ehai.common.utils.SignUtils;
-
+import com.alibaba.fastjson.JSON;
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -20,32 +19,21 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-
 import javax.servlet.http.HttpServletResponse;
-
-import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
 /**
- * @Description:通用拦截器处理参数不能二次读取问题
- * @author:lixiao
- * @time:2017年11月10日 下午4:46:07
+ * 验证加密、签名返回参数过滤器
+ *
+ * @author lixiao
+ * @date 2019-02-12 16:00
  */
-@Order(1)
+@Order(100)
 @Configuration
-@WebFilter(filterName = "CommonFilter", urlPatterns = "/**")
-@ConditionalOnProperty(
-        prefix = "project",
-        value = "sign",
-        havingValue = "true"
-)
-public class CommonFilter implements Filter {
-
+public class VerificationResFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -53,41 +41,24 @@ public class CommonFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        ServletRequest requestWrapper = null;
-		try{
-			if (request instanceof HttpServletRequest) {
-				requestWrapper = new EhiHttpServletRequestWrapper((HttpServletRequest) request);
-			}
-		}catch (Exception e){
-			responseResult((HttpServletResponse)response, ResultGenerator.genFailResult(ResultCode.UNAUTHORIZED, "签名错误"));
-			return;
-		}
+        throws IOException, ServletException {
 
         EhiHttpServletResponseWrapper contentCachingResponseWrapper = new EhiHttpServletResponseWrapper(
-                (HttpServletResponse) response);
-
-        if (requestWrapper == null) {
-            chain.doFilter(request, response);
-        } else {
+            (HttpServletResponse) response);
             try {
-                chain.doFilter(requestWrapper, contentCachingResponseWrapper);
+                chain.doFilter(request, contentCachingResponseWrapper);
             } catch (Exception e) {
                 boolean isLogger = !(e instanceof ServiceException);
                 if (isLogger) {
                     LoggerUtils.error(getClass(), new EHIExceptionLogstashMarker(new EHIExceptionMsgWrapper(getClass()
-                            .getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new
-                            Object[]{request, response, chain}, ExceptionUtils.getStackTrace(e))));
+                        .getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new
+                        Object[]{request, response, chain}, ExceptionUtils.getStackTrace(e))));
                 }
             } finally {
-
                 ServletOutputStream out;
                 try {
                     String respStr = IOUtils.getResponseBody(contentCachingResponseWrapper.getContent());
-
-
                     byte[] aesResp = AESUtils.aesEncryptString(respStr).getBytes("UTF-8");
-
                     String resSign = SignUtils.signResponse(respStr);
                     contentCachingResponseWrapper.setHeader("x-ehi-sign", resSign);
                     contentCachingResponseWrapper.setHeader("content-type", "text");
@@ -98,29 +69,16 @@ public class CommonFilter implements Filter {
 
                 } catch (Exception e) {
                     LoggerUtils.error(getClass(), new EHIExceptionLogstashMarker(new EHIExceptionMsgWrapper(getClass()
-                            .getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new
-                            Object[]{request, response, chain}, ExceptionUtils.getStackTrace(e))));
+                        .getName(), Thread.currentThread().getStackTrace()[1].getMethodName(), new
+                        Object[]{request, response, chain}, ExceptionUtils.getStackTrace(e))));
                 } finally {
 
                 }
             }
-        }
-
-
     }
 
     @Override
     public void destroy() {
 
     }
-	private void responseResult(HttpServletResponse response, Result result) {
-		response.setCharacterEncoding("UTF-8");
-		response.setHeader("Content-type", "application/json;charset=UTF-8");
-		response.setStatus(200);
-		try {
-			response.getWriter().write(JSON.toJSONString(result));
-
-		} catch (IOException ex) {
-		}
-	}
 }
