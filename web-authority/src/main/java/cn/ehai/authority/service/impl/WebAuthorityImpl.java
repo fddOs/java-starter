6def1a33292474e4f4f6c6fe2a669bf4f200f6e1
xianglong.chen✨windows <xianglong.chen@1hai.cn>
2019-04-15 10:48:02 +0800

    feat(权限验证): 完善接口多权限的验证功能

--- web-authority/src/main/java/cn/ehai/authority/service/impl/WebAuthorityImpl.java
+++ web-authority/src/main/java/cn/ehai/authority/service/impl/WebAuthorityImpl.java
@@ -2,7 +2,10 @@ package cn.ehai.authority.service.impl;
 
 import cn.ehai.authority.http.api.AuthApi;
 import cn.ehai.authority.service.WebAuthority;
+import cn.ehai.common.core.ApolloBaseConfig;
 import cn.ehai.common.core.Result;
+import feign.Feign;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
@@ -17,8 +20,15 @@ import java.util.concurrent.Future;
 @Service
 public class WebAuthorityImpl implements WebAuthority {
 
-    @Autowired
     private AuthApi authApi;
+
+    public WebAuthorityImpl(Feign.Builder builder) {
+        String authUrl = ApolloBaseConfig.getAuthUrl();
+        if (StringUtils.isEmpty(authUrl)) {
+            throw new IllegalArgumentException("authUrl参数获取失败，请在ApolloCommonConfig中检查该配置项");
+        }
+        authApi = builder.target(AuthApi.class, authUrl);
+    }
 
     @Override
     public Result<Boolean> verifyAuth(String userCode, String systemCode, String moduleId) {
