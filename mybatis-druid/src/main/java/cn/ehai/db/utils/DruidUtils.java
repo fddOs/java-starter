8c403b30027c2a6df69c85b6562de6b00c79f3ff
lixiao <394424129@qq.com>
2019-04-18 14:58:33 +0800

    feat(0.7.1): druid
    去掉数据库的driverclasss

--- mybatis-druid/src/main/java/cn/ehai/db/utils/DruidUtils.java
+++ mybatis-druid/src/main/java/cn/ehai/db/utils/DruidUtils.java
@@ -25,7 +25,6 @@ public class DruidUtils {
      */
     public static  DruidDataSource getDataSource(DBInfo dbInfo) {
         DruidDataSource dataSource = DruidDataSourceBuilder.create().build();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl(dbInfo.getUrl());
         dataSource.setUsername(dbInfo.getUserName());
         dataSource.setPassword(dbInfo.getPassword());
@@ -45,7 +44,6 @@ public class DruidUtils {
     public static void restart (DruidDataSource dataSource,DBInfo dbInfo, List<String> initSql) {
         try {
             dataSource.restart();
-            dataSource.setDriverClassName("com.mysql.jdbc.Driver");
             dataSource.setUrl(dbInfo.getUrl());
             dataSource.setUsername(dbInfo.getUserName());
             dataSource.setPassword(dbInfo.getPassword());
