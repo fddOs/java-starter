3c960b376ed3b1e49d845f8a0902950319696e1a
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-09-09 10:05:51 +0800

    feat: 优化Feign网络请求脱壳的功能, json 处理用 fastjson 处理
    优化Feign网络请求脱壳的功能, json 处理用 fastjson 处理

--- rpc-feign/src/main/java/cn/seed/rpc/feign/decoder/RemoveShellDecoder.java
+++ rpc-feign/src/main/java/cn/seed/rpc/feign/decoder/RemoveShellDecoder.java
@@ -3,11 +3,11 @@ package cn.seed.rpc.feign.decoder;
 import cn.seed.common.core.ResultCode;
 import cn.seed.rpc.annotation.RemoveShell;
 import cn.seed.rpc.feign.RemoveShellException;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import feign.Response;
 import feign.Util;
 import feign.codec.Decoder;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
@@ -49,25 +49,25 @@ public class RemoveShellDecoder implements Decoder {
 
         try {
             String jsonBody = Util.toString(response.body().asReader());
-            JSONObject jb = new JSONObject(jsonBody);
+            JSONObject jb = JSON.parseObject(jsonBody);
             // 兼容两种, 新的老的
-            int errorCode = jb.optInt("errorCode", Integer.MIN_VALUE);
-            int code = jb.optInt("code", Integer.MIN_VALUE);
+            Integer errorCode = jb.getInteger("errorCode");
+            Integer code = jb.getInteger("code");
             // 如果两个都是没有, 说明数据结构不符合逻辑, 直接抛出异常
-            if (errorCode == Integer.MIN_VALUE && code == Integer.MIN_VALUE) {
+            if (errorCode == null && code == null) {
                 throw new RemoveShellException(ResultCode.HTTP_FAIL, "返回的 json 数据结构不满足 @RemoveShell 注解的脱壳要求");
             }
             // 错误信息, 可能为空
-            String message = jb.optString("message");
+            String message = jb.getString("message");
             // 用户关心的数据
             String realData = null;
             // 表示成功
-            if (errorCode == ResultCode.SUCCESS.getCode()) {
-                realData = jb.optString("result");
-            } else if (code == 200) {
-                realData = jb.optString("data");
+            if (errorCode != null && errorCode == ResultCode.SUCCESS.getCode()) {
+                realData = jb.getString("result");
+            } else if (code != null && code == 200) {
+                realData = jb.getString("data");
             } else { // 其他的情况都是异常情况
-                throw new RemoveShellException(errorCode == Integer.MIN_VALUE ? code : errorCode, message);
+                throw new RemoveShellException(errorCode == null ? code : errorCode, message);
             }
 
             // ================= 到这里业务请求是正确的, 现在开始处理 Result 中的 result 为空的情况 =================
@@ -89,7 +89,7 @@ public class RemoveShellDecoder implements Decoder {
                     .build();
             // 脱壳完成
             return nextDecoder.decode(nextResponse, type);
-        } catch (JSONException e) {
+        } catch (Exception e) {
             throw new RemoveShellException(ResultCode.HTTP_FAIL, "Response 脱壳失败", e);
         }
 
