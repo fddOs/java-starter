a804a13971ace1a1c5131578d58580adb12b28f3
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-09-09 15:05:32 +0800

    feat: 优化注释的内容, 修复生成文档报错的注释
    优化注释的内容, 修复生成文档报错的注释

--- rpc-feign/src/main/java/cn/seed/rpc/annotation/RemoveShell.java
+++ rpc-feign/src/main/java/cn/seed/rpc/annotation/RemoveShell.java
@@ -6,54 +6,61 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.METHOD;
 
 /**
  * 此注解标记在请求的接口的方法上, 目前我们使用的网络请求库为 {@link feign.Feign}
  * 用于帮助使用者脱掉外层统一封装的 {@link cn.seed.common.core.Result}
  * 不使用注解的时候, 你需要如下使用
- * <pre>{@code
- *     Result<User> getUserByUserId(@NotNull String userId);
- * }</pre>
+ * <pre>
+ *
+ *      Result&lt;User&gt; getUserByUserId(@NotNull String userId);
+ * </pre>
  * 当你使用 {@link RemoveShell} 注解, 你可以这样子使用
- * <pre>{@code
- *      @RemoveShell
+ * <pre>
+ *
+ *      &#64;RemoveShell
  *      User getUserByUserId(@NotNull String userId);
- * }</pre>
- * <p>
+ * </pre>
  * 使用了此注解可以有两个好处：
- * 1. 在接口声明的时候, 不需要每次都写那个固定的 {@link cn.seed.common.core.Result},
+ * <ol>
+ * <li> 在接口声明的时候, 不需要每次都写那个固定的 {@link cn.seed.common.core.Result},
  * 你可以让返回值是你原本的对象 {@link Result#getResult()}
- * 2. 在使用上, 你可以对比以下两段代码
- * <pre>{@code
- *        // 使用前
- *        Result<User> result = xxx.getUserByUserId(5);
- *        if(result.getErrorCode() == 0){
- *              User user = result.getResult();
- *        }
- *        // 使用后
- *        User user = xxx.getUserByUserId(5);
- * }</pre>
- * <p>
- * Note: 使用此功能, 会有个地方需要关注下, 以前 json 是映射整个 {@link cn.seed.common.core.Result}
+ * <li> 在使用上, 你可以对比以下两段代码
+ * <pre>
+ *
+ *      // 使用前
+ *      Result&lt;User&gt; result = xxx.getUserByUserId(5);
+ *      if(result.getErrorCode() == 0){
+ *          User user = result.getResult();
+ *      }
+ *      // 使用后
+ *      User user = xxx.getUserByUserId(5);
+ * </pre>
+ * </ol>
+ * 使用此功能, 会有个地方需要关注下, 以前 json 是映射整个 {@link cn.seed.common.core.Result}
  * 现在可以映射内部的 {@link Result#getResult()}, 所以有可能会产生一个情况
  * 当后台返回如下的格式的数据时, 内部的 result 是 null 或者是一个 "", 那么这时候是否允许为空会是一个问题
+ * <pre>
  * {
- * errorCode : 0,
- * message : "ok",
- * result : null
+ *      errorCode : 0,
+ *      message : "ok",
+ *      result : null
  * }
- * <p>
+ * </pre>
  * 所以注解中添加了一个参数 {@link RemoveShell#allowEmpty()} 来控制是否允许为空
  * <p>
- * 如下代码就可允许 User 返回 null, 否则你将得到一个异常 {@link cn.seed.rpc.feign.RemoveShellException}
- * <pre>{@code
- *      @RemoveShell(allowEmpty = true)
+ * 如下代码就可允许 {@code User} 返回 {@code null}, 否则你将得到一个异常 {@link cn.seed.rpc.feign.RemoveShellException}
+ * <pre>
+ *      &#64;RemoveShell(allowEmpty = true)
  *      User getUserByUserId(@NotNull String userId);
- * }</pre>
+ * </pre>
+ *
+ * @author xiaojinzi
+ * @since 0.7.6
  */
-@Retention(RetentionPolicy.RUNTIME)
 @Target(value = {METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RemoveShell {
 
     /**
